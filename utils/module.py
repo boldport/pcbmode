9ab10dfafe2251082c881ee25191cb18f44144e2
Saar Drimer <saardrimer@gmail.com>
2014-11-30 18:44:05 +0000

    Do not invert solderpaste pad (twice)

--- utils/module.py
+++ utils/module.py
@@ -427,7 +427,7 @@ class Module():
                     group = et.SubElement(svg_layer, 'g', transform=transform)
                     group.set('{'+config.cfg['ns']['pcbmode']+'}type', 'component-shapes')
                     for shape in shapes:
-                        placed_element = place.placeShape(shape, group, invert)
+                        placed_element = place.placeShape(shape, group)
 
 
                 # Silkscreen
