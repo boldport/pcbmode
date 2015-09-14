90a185e2525784e30156129a40810db4e52abb63
saar drimer <saardrimer@gmail.com>
2015-09-14 22:51:09 +0100

    Cleanup

--- utils/module.py
+++ utils/module.py
@@ -325,11 +325,10 @@ class Module():
                     svg_layer = None
 
                 if len(shapes) > 0 and svg_layer != None:
-                    print 'FFF'
                     transform = "translate(%s,%s)" % (location[0],
                                                       config.cfg['invert-y']*location[1])
                     group = et.SubElement(shape_group, 'g', transform=transform)
-                    #group.set('{'+config.cfg['ns']['pcbmode']+'}type', 'component-shapes')
+                    group.set('{'+config.cfg['ns']['pcbmode']+'}type', 'pours')
                     for shape in shapes:
                         placed_element = place.placeShape(shape, group, invert)
 
