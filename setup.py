a837a052aa3d60b2c969a8ad3ab5b7344f83107a
Saar Drimer <saardrimer@gmail.com>
2019-12-30 16:15:57 +0000

    Update

--- setup.py
+++ setup.py
@@ -1,30 +1,38 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
+
 setup(
     name = "pcbmode",
-    packages = find_packages(),
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
+    description = "A printed circuit board design tool with a twist",
+    long_description = "",
+    version = "5.0.0a1",
+    license = "GPL 3.0",
+    url = "https://pcbmode.com",
 
-    install_requires = ['lxml', 'pyparsing'],
-
-    package_data = {
-        'pcbmode': ['stackups/*.json',
-                    'styles/*/*.json',
-                    'fonts/*.svg',
-                    'pcbmode_config.json'],
-    },
-
-    # metadata for upload to PyPI
     author = "Saar Drimer",
     author_email = "saardrimer@gmail.com",
-    description = "A printed circuit board design tool with a twist",
-    license = "MIT",
     keywords = "pcb svg eda pcbmode",
-    url = "https://github.com/boldport/pcbmode",
+
+    python_requires = ">=3.7.0",
+
+    packages=find_namespace_packages(include=['pcbmode.*']),
+
+    install_requires = [
+        'lxml>=4.4.0',
+        'pyparsing>=2.4.2'
+    ],
+
+    package_data = {
+        'pcbmode': [
+            'stackups/*.json',
+            'styles/*/*.json',
+            'fonts/*.svg',
+            'pcbmode_config.json'
+        ],
+    },
 
     entry_points={
-        'console_scripts': ['pcbmode = pcbmode.pcbmode:main']
-    },
-    zip_safe = True
+        'console_scripts': [
+            'pcbmode = pcbmode.pcbmode:main'
+        ]
+    }
 )
-
