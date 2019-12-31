3a12b93c084b78763dc920ae8155ca8bf2456e6c
Saar Drimer <saardrimer@gmail.com>
2019-12-31 20:05:32 +0000

    Cleanup

--- pcbmode/utils/svg_path_grammar.py
+++ pcbmode/utils/svg_path_grammar.py
@@ -21,58 +21,51 @@ import pyparsing as PP
 
 
 def get_grammar():
-    """ Return the pyparsing grammar for an SVG path
+    """ 
+    Return the pyparsing grammar for an SVG path
     """
 
-    comma = PP.Literal(",").suppress()  # supress removes the ',' when captured
-    dot = PP.Literal(".")
-    space = PP.Literal(" ")
+    comma = PP.Literal(",").suppress() # supress doesn't capture the 'comma
     coord = PP.Regex(r"-?\d+(\.\d*)?([Ee][+-]?\d+)?")
-    one_coord = PP.Group(coord)
-    xycoords = PP.Group(coord + PP.Optional(comma) + coord)
-    two_xycoords = xycoords + PP.Optional(comma) + xycoords
-    three_xycoords = (
-        xycoords + PP.Optional(comma) + xycoords + PP.Optional(comma) + xycoords
-    )
+    coord_x1 = PP.Group(coord)
+    xy_x1 = PP.Group(coord + PP.Optional(comma) + coord)
+    xy_x2 = (xy_x1 + PP.Optional(comma))*2
+    xy_x3 = (xy_x1 + PP.Optional(comma))*3
 
-    c_M = PP.Literal("M") + PP.OneOrMore(xycoords)
-    c_m = PP.Literal("m") + PP.OneOrMore(xycoords)
-    c_C = PP.Literal("C") + PP.OneOrMore(three_xycoords)
-    c_c = PP.Literal("c") + PP.OneOrMore(three_xycoords)
-    c_Q = PP.Literal("Q") + PP.OneOrMore(two_xycoords)
-    c_q = PP.Literal("q") + PP.OneOrMore(two_xycoords)
-    c_T = PP.Literal("T") + PP.OneOrMore(xycoords)
-    c_t = PP.Literal("t") + PP.OneOrMore(xycoords)
-    c_L = PP.Literal("L") + PP.OneOrMore(xycoords)
-    c_l = PP.Literal("l") + PP.OneOrMore(xycoords)
-    c_V = PP.Literal("V") + PP.OneOrMore(one_coord)
-    c_v = PP.Literal("v") + PP.OneOrMore(one_coord)
-    c_H = PP.Literal("H") + PP.OneOrMore(one_coord)
-    c_h = PP.Literal("h") + PP.OneOrMore(one_coord)
-    c_S = PP.Literal("S") + PP.OneOrMore(two_xycoords)
-    c_s = PP.Literal("s") + PP.OneOrMore(two_xycoords)
+    # Note to future self trying to potimise (from pp docs):
+    # CaselessLiteral - construct with a string to be matched, but without case
+    # checking;results are always returned as the defining literal, NOT as they are 
+    # found in the input string
+
+    c_M = PP.Literal("M") + PP.OneOrMore(xy_x1)
+    c_m = PP.Literal("m") + PP.OneOrMore(xy_x1)
+    c_C = PP.Literal("C") + PP.OneOrMore(xy_x3)
+    c_c = PP.Literal("c") + PP.OneOrMore(xy_x3)
+    c_Q = PP.Literal("Q") + PP.OneOrMore(xy_x2)
+    c_q = PP.Literal("q") + PP.OneOrMore(xy_x2)
+    c_T = PP.Literal("T") + PP.OneOrMore(xy_x1)
+    c_t = PP.Literal("t") + PP.OneOrMore(xy_x1)
+    c_L = PP.Literal("L") + PP.OneOrMore(xy_x1)
+    c_l = PP.Literal("l") + PP.OneOrMore(xy_x1)
+    c_V = PP.Literal("V") + PP.OneOrMore(coord_x1)
+    c_v = PP.Literal("v") + PP.OneOrMore(coord_x1)
+    c_H = PP.Literal("H") + PP.OneOrMore(coord_x1)
+    c_h = PP.Literal("h") + PP.OneOrMore(coord_x1)
+    c_S = PP.Literal("S") + PP.OneOrMore(xy_x2)
+    c_s = PP.Literal("s") + PP.OneOrMore(xy_x2)
     c_z = PP.Literal("z")
     c_Z = PP.Literal("Z")
 
     path_cmd = (
-        c_M
-        | c_m
-        | c_C
-        | c_c
-        | c_Q
-        | c_q
-        | c_T
-        | c_t
-        | c_L
-        | c_l
-        | c_V
-        | c_v
-        | c_H
-        | c_h
-        | c_S
-        | c_s
-        | c_Z
-        | c_z
+          c_M | c_m
+        | c_C | c_c
+        | c_Q | c_q
+        | c_T | c_t
+        | c_L | c_l
+        | c_V | c_v
+        | c_H | c_h
+        | c_S | c_s
+        | c_Z | c_z
     )
 
     return PP.OneOrMore(PP.Group(path_cmd))
