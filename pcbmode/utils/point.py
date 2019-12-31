bd0101d1934206f898f885047ec92be9a081c508
Saar Drimer <saardrimer@gmail.com>
2019-12-31 23:08:51 +0000

    Black and license header

--- pcbmode/utils/point.py
+++ pcbmode/utils/point.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
 
+# PCBmodE, a printed circuit design software with a twist
+# Copyright (C) 2020 Saar Drimer
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 from math import pi, sin, cos
 import decimal
 
@@ -10,21 +27,20 @@ DEG2RAD = 2 * pi / 360
 
 
 class Point:
-  
     def __init__(self, x=0, y=0):
         try:
-            self.sig_dig = config.cfg['params']['significant-digits']
+            self.sig_dig = config.cfg["params"].get("significant-digits") or 8
         except:
             self.sig_dig = 8
         self.x = round(float(x), self.sig_dig)
         self.y = round(float(y), self.sig_dig)
-       
+
     def __add__(self, p):
-        """ add point 'p' of type Point to current point""" 
+        """ add point 'p' of type Point to current point"""
         return Point(self.x + p.x, self.y + p.y)
 
     def __sub__(self, p):
-        """ subtract point 'p' of type Point to current point""" 
+        """ subtract point 'p' of type Point to current point"""
         return Point(self.x - p.x, self.y - p.y)
 
     def __repr__(self, d=2):
@@ -40,11 +56,11 @@ class Point:
 
     def __ne__(self, p):
         """ not equal attribute """
-        return not((self.x == p.x) and (self.y == p.y))
+        return not ((self.x == p.x) and (self.y == p.y))
 
     def assign(self, x=0, y=0):
-        self.x = round(float(x), self.sig_dig) 
-        self.y = round(float(y), self.sig_dig) 
+        self.x = round(float(x), self.sig_dig)
+        self.y = round(float(y), self.sig_dig)
         return
 
     def rotate(self, deg, p):
@@ -52,8 +68,8 @@ class Point:
         rad = deg * DEG2RAD
         x = self.x
         y = self.y
-        self.x = (x * cos(rad) + y * sin(rad))
-        self.y = (x * -sin(rad) + y * cos(rad))
+        self.x = x * cos(rad) + y * sin(rad)
+        self.y = x * -sin(rad) + y * cos(rad)
         return
 
     def round(self, d):
--- pcbmode/utils/style.py
+++ pcbmode/utils/style.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python
 
+# PCBmodE, a printed circuit design software with a twist
+# Copyright (C) 2020 Saar Drimer
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 from pcbmode.config import config
 from pcbmode.utils import messages as msg
 from pcbmode.utils import utils
 
 
-class Style():
+class Style:
     """
     Manages the logic for determining the style of an object
     based on its shape definition, 'shape_dict'. In the layout
@@ -17,53 +34,52 @@ class Style():
     Originally it was added for 'refdef' within silkscreen and
     soldermask, but could be used with other types.
     """
+
     def __init__(self, shape_dict, layer_name, sub_item=None):
 
-        default_style = config.stl['layout']['defaults']['style'][layer_name]
+        default_style = config.stl["layout"]["defaults"]["style"][layer_name]
         if sub_item == None:
-            layer_style = config.stl['layout'][layer_name]
+            layer_style = config.stl["layout"][layer_name]
         else:
-            layer_style = config.stl['layout'][layer_name][sub_item]
+            layer_style = config.stl["layout"][layer_name][sub_item]
 
         # Unless specified, 'text' will default to 'fill' on all layers.
         # Other 'types' depend on the layers they are on.
-        if shape_dict.get('style') == None:
-            if shape_dict['type'] in ['text', 'refdef']:
-                self._style = 'fill'
+        if shape_dict.get("style") == None:
+            if shape_dict["type"] in ["text", "refdef"]:
+                self._style = "fill"
             else:
                 self._style = default_style
         else:
-            self._style = shape_dict['style']
+            self._style = shape_dict["style"]
 
         try:
             self._style_dict = layer_style.get(self._style).copy()
         except:
             self._style_dict = None
-            
+
         # Apply defaults if style dict wasn't found
         if self._style_dict == None:
-            if self._style == 'fill':
+            if self._style == "fill":
                 self._style_dict = {"stroke": "none"}
-            elif self._style == 'stroke':
+            elif self._style == "stroke":
                 self._style_dict = {"fill": "none"}
             else:
                 msg.error("Encountered an unknown 'style' type, %s" % self._style)
 
-        # If the style is 'stroke' we need to override the default 'stroke-width' 
+        # If the style is 'stroke' we need to override the default 'stroke-width'
         # setting with a possible custom definition
-        if self._style == 'stroke':
-            self._style_dict['stroke-width'] = (shape_dict.get('stroke-width') or
-                                                layer_style[self._style].get('stroke-width'))
-            self._style_dict['fill'] = "none"
-
+        if self._style == "stroke":
+            self._style_dict["stroke-width"] = shape_dict.get(
+                "stroke-width"
+            ) or layer_style[self._style].get("stroke-width")
+            self._style_dict["fill"] = "none"
 
     def getStyleType(self):
         return self._style
 
-
     def getStyleString(self):
         return utils.dictToStyleText(self._style_dict)
 
-
     def getStrokeWidth(self):
-        return self._style_dict['stroke-width']
+        return self._style_dict["stroke-width"]
