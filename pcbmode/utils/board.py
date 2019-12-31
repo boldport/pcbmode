c56b874def5eb4a2c25c9370729b0f292d1e76be
Saar Drimer <saardrimer@gmail.com>
2019-12-31 23:02:02 +0000

    Black and license header

--- pcbmode/utils/board.py
+++ pcbmode/utils/board.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python
 
+# PCBmodE, a printed circuit design software with a twist
+# Copyright (C) 2020 Saar Drimer
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 from pcbmode.config import config
 from pcbmode.utils import messages as msg
 from pcbmode.utils.module import Module
 
 
-
-class Board():
+class Board:
     """
     """
 
     def __init__(self):
-
         self._module_dict = config.brd
         self._module_routing = config.rte
-        module = Module(self._module_dict,
-                        self._module_routing)
-
-
-
+        module = Module(self._module_dict, self._module_routing)
