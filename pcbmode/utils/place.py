a32666b7c7ae35deabdcd71027d56cd20b9052f1
Saar Drimer <saardrimer@gmail.com>
2020-01-01 23:54:41 +0000

    Black and license header

--- pcbmode/utils/place.py
+++ pcbmode/utils/place.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
 
+# PCBmodE, a printed circuit design software with a twist
+# Copyright (C) 2020 Saar Drimer
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 from lxml import etree as et
 
 from pcbmode.config import config
@@ -9,9 +26,6 @@ from pcbmode.utils import svg
 from pcbmode.utils.point import Point
 
 
-
-
-
 def placeShape(shape, svg_layer, invert=False, original=False):
     """
     Places a shape or type 'Shape' onto SVG layer 'svg_layer'.
@@ -19,7 +33,7 @@ def placeShape(shape, svg_layer, invert=False, original=False):
     'original': use the original path, not the transformed one
     """
 
-    sig_dig = config.cfg['params']['significant-digits']
+    sig_dig = config.cfg["params"]["significant-digits"]
 
     style_string = shape.getStyleString()
     style_type = shape.getStyleType()
@@ -27,8 +41,10 @@ def placeShape(shape, svg_layer, invert=False, original=False):
     location = shape.getLocation()
 
     if original == False:
-        translate = 'translate(%s,%s)' % (round((((1,-1)[invert])*location.x), sig_dig),
-                                          round(location.y*config.cfg['iya'], sig_dig))
+        translate = "translate(%s,%s)" % (
+            round((((1, -1)[invert]) * location.x), sig_dig),
+            round(location.y * config.cfg["iya"], sig_dig),
+        )
         transform = translate
     else:
         transform = None
@@ -41,126 +57,149 @@ def placeShape(shape, svg_layer, invert=False, original=False):
         else:
             path = shape.getTransformedPath()
 
-    element = et.SubElement(svg_layer, 
-                            'path',
-                            d=path)
+    element = et.SubElement(svg_layer, "path", d=path)
     # Set style string
-    element.set('style', style_string)
+    element.set("style", style_string)
 
     # Set style type in pcbmode namespace. This is later used to easliy
     # identify the type when the path is converted to Gerber format
-    element.set('{'+config.cfg['ns']['pcbmode']+'}style', style_type)
+    element.set("{" + config.cfg["ns"]["pcbmode"] + "}style", style_type)
 
     if transform != None:
-        element.set('transform', transform)
+        element.set("transform", transform)
 
     if gerber_lp != None:
-        element.set('{'+config.cfg['ns']['pcbmode']+'}gerber-lp', gerber_lp)
+        element.set("{" + config.cfg["ns"]["pcbmode"] + "}gerber-lp", gerber_lp)
 
-    if shape.getType() == 'text':
-        element.set('{'+config.cfg['ns']['pcbmode']+'}text', shape.getText())
+    if shape.getType() == "text":
+        element.set("{" + config.cfg["ns"]["pcbmode"] + "}text", shape.getText())
 
     return element
 
 
-
-
-
-
-def placeDrill(drill, 
-               layer, 
-               location, 
-               scale, 
-               soldermask_layers={}, 
-               mask_groups={}):
+def placeDrill(drill, layer, location, scale, soldermask_layers={}, mask_groups={}):
     """
     Places the drilling point
     """
 
-    diameter = drill.get('diameter')
-    offset = utils.to_Point(drill.get('offset') or [0, 0])
+    diameter = drill.get("diameter")
+    offset = utils.to_Point(drill.get("offset") or [0, 0])
     path = svg.drill_diameter_to_path(diameter)
     mask_path = svg.circle_diameter_to_path(diameter)
 
-    sig_dig = config.cfg['params']['significant-digits']
-    transform = 'translate(%s %s)' % (round((location.x + offset.x)*scale, sig_dig),
-                                      round((-location.y - offset.y)*scale, sig_dig))
+    sig_dig = config.cfg["params"]["significant-digits"]
+    transform = "translate(%s %s)" % (
+        round((location.x + offset.x) * scale, sig_dig),
+        round((-location.y - offset.y) * scale, sig_dig),
+    )
 
-    drill_element = et.SubElement(layer, 'path',
-                                  transform=transform,
-                                  d=path,
-                                  id='pad_drill',
-                                  diameter=str(diameter))
+    drill_element = et.SubElement(
+        layer,
+        "path",
+        transform=transform,
+        d=path,
+        id="pad_drill",
+        diameter=str(diameter),
+    )
 
-    pour_buffer = board_cfg['params']['distances']['from_pour_to']['drill']
+    pour_buffer = board_cfg["params"]["distances"]["from_pour_to"]["drill"]
 
     # add a mask buffer between pour and board outline
     if mask_groups != {}:
         for pcb_layer in surface_layers:
-            mask_group = et.SubElement(mask_groups[pcb_layer], 'g',
-                                            id="drill_masks")
-            pour_mask = et.SubElement(mask_group, 'path',
-                                      transform=transform,
-                                      style=MASK_STYLE % str(pour_buffer*2),
-                                      gerber_lp="c",
-                                      d=mask_path)
-
-
+            mask_group = et.SubElement(mask_groups[pcb_layer], "g", id="drill_masks")
+            pour_mask = et.SubElement(
+                mask_group,
+                "path",
+                transform=transform,
+                style=MASK_STYLE % str(pour_buffer * 2),
+                gerber_lp="c",
+                d=mask_path,
+            )
 
     # place the size of the drill; id the drill element has a
     # "show_diameter": "no", then this can be suppressed
     # default to 'yes'
-    show_diameter = drill.get('show_diameter') or 'yes'
-    if show_diameter.lower() != 'no':
+    show_diameter = drill.get("show_diameter") or "yes"
+    if show_diameter.lower() != "no":
         text = "%s mm" % (str(diameter))
-        text_style = config.stl['layout']['drills'].get('text') or None
+        text_style = config.stl["layout"]["drills"].get("text") or None
         if text_style is not None:
-            text_style['font-size'] = str(diameter/10.0)+'px'
+            text_style["font-size"] = str(diameter / 10.0) + "px"
             text_style = utils.dict_to_style(text_style)
-            t = et.SubElement(layer, 'text',
+            t = et.SubElement(
+                layer,
+                "text",
                 x=str(location.x),
                 # TODO: get rid of this hack
-                y=str(-location.y-(diameter/4)),
-                style=text_style)
+                y=str(-location.y - (diameter / 4)),
+                style=text_style,
+            )
             t.text = text
 
     # place soldermask unless specified otherwise
     # default is 'yes'
-    add_soldermask = drill.get('add_soldermask') or 'yes'
-    style = utils.dict_to_style(config.stl['layout']['soldermask'].get('fill'))
-    possible_answers = ['yes', 'top', 'top only', 'bottom', 'bottom only', 'top and bottom']
+    add_soldermask = drill.get("add_soldermask") or "yes"
+    style = utils.dict_to_style(config.stl["layout"]["soldermask"].get("fill"))
+    possible_answers = [
+        "yes",
+        "top",
+        "top only",
+        "bottom",
+        "bottom only",
+        "top and bottom",
+    ]
     if (add_soldermask.lower() in possible_answers) and (soldermask_layers != {}):
         # TODO: get this into a configuration parameter
-        drill_soldermask_scale_factors = drill.get('soldermask_scale_factors') or {'top':1.2, 'bottom':1.2}
-        path_top = svg.circle_diameter_to_path(diameter * drill_soldermask_scale_factors['top'])
-        path_bottom = svg.circle_diameter_to_path(diameter * drill_soldermask_scale_factors['bottom'])
+        drill_soldermask_scale_factors = drill.get("soldermask_scale_factors") or {
+            "top": 1.2,
+            "bottom": 1.2,
+        }
+        path_top = svg.circle_diameter_to_path(
+            diameter * drill_soldermask_scale_factors["top"]
+        )
+        path_bottom = svg.circle_diameter_to_path(
+            diameter * drill_soldermask_scale_factors["bottom"]
+        )
 
-        if add_soldermask.lower() == 'yes' or add_soldermask.lower() == 'top and bottom':
-            drill_element = et.SubElement(soldermask_layers['top'], 
-                                          'path',
-                                          transform=transform,
-                                          style=style,
-                                          d=path_top)
-            drill_element = et.SubElement(soldermask_layers['bottom'], 
-                                          'path',
-                                          transform=transform,
-                                          style=style,
-                                          d=path_bottom)
-        elif add_soldermask.lower() == 'top only' or add_soldermask.lower() == 'top':
-            drill_element = et.SubElement(soldermask_layers['top'], 
-                                          'path',
-                                          transform=transform,
-                                          style=style,
-                                          d=path_top)
-        elif add_soldermask.lower() == 'bottom only' or add_soldermask.lower() == 'bottom':
-            drill_element = et.SubElement(soldermask_layers['bottom'], 
-                                          'path',
-                                          transform=transform,
-                                          style=style,
-                                          d=path_bottom)
+        if (
+            add_soldermask.lower() == "yes"
+            or add_soldermask.lower() == "top and bottom"
+        ):
+            drill_element = et.SubElement(
+                soldermask_layers["top"],
+                "path",
+                transform=transform,
+                style=style,
+                d=path_top,
+            )
+            drill_element = et.SubElement(
+                soldermask_layers["bottom"],
+                "path",
+                transform=transform,
+                style=style,
+                d=path_bottom,
+            )
+        elif add_soldermask.lower() == "top only" or add_soldermask.lower() == "top":
+            drill_element = et.SubElement(
+                soldermask_layers["top"],
+                "path",
+                transform=transform,
+                style=style,
+                d=path_top,
+            )
+        elif (
+            add_soldermask.lower() == "bottom only"
+            or add_soldermask.lower() == "bottom"
+        ):
+            drill_element = et.SubElement(
+                soldermask_layers["bottom"],
+                "path",
+                transform=transform,
+                style=style,
+                d=path_bottom,
+            )
         else:
             print("ERROR: unrecognised drills soldermask option")
 
     return
-
-
