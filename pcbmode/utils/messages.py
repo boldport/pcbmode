97053c49d0c71fbac7589d2096470530b77247f5
Saar Drimer <saardrimer@gmail.com>
2020-01-02 00:03:28 +0000

    Black and license header

--- pcbmode/utils/messages.py
+++ pcbmode/utils/messages.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
 
+# PCBmodE, a printed circuit design software with a twist
+# Copyright (C) 2020 Saar Drimer
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import sys
 
 
@@ -21,7 +38,6 @@ def note(note, newline=True):
         sys.stdout.write("-- NOTE: %s" % note)
 
 
-
 def subInfo(info, newline=True):
     """
     """
@@ -31,27 +47,22 @@ def subInfo(info, newline=True):
         sys.stdout.write(" * %s" % info)
 
 
-
 def error(info, error_type=None):
     """
     Print a message and then raise
     """
 
-    print('')
-    print('-----------------------------')
-    print('Yikes, ERROR!')
-    print('')
-    print('* %s' % info)
-    print('')
-    print('Solder on!')
-    print('-----------------------------')
-    print('')
+    print("")
+    print("-----------------------------")
+    print("Yikes, ERROR!")
+    print("")
+    print("* %s" % info)
+    print("")
+    print("Solder on!")
+    print("-----------------------------")
+    print("")
 
     if error_type != None:
         raise error_type
     else:
         raise Exception
-
-
-
-
