d05f4b7e9c9a73acea40c94127f64eb8a4817105
Saar Drimer <saardrimer@gmail.com>
2020-01-03 19:47:19 +0000

    Initial commit

---
+++ pcbmode/utils/lxml_utils.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+# PCBmodE, a printed circuit design software with a twist
+# Copyright (C) 2020 Saar Drimer
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+from lxml import etree as et
+
+
+def addch(parent, ns, name, id=None, text=None):
+    """
+    Add a child element to parent
+    """
+    new_child = et.SubElement(parent, "{{{}}}{}".format(ns, name))
+    if id is not None:
+        new_child.set("id", id)
+    if text is not None:
+        new_child.text = text
+    return new_child
