24e644ad439bb215b22566caa1bd13d825a8126d
Saar Drimer <saardrimer@gmail.com>
2020-01-07 17:02:07 +0000

    Cleanup

--- pcbmode/utils/svg_layers.py
+++ pcbmode/utils/svg_layers.py
@@ -24,35 +24,29 @@ from pcbmode.config import config
 def create_layers(parent_el, transform=None, refdef=None):
     """
     Creates Inkscape SVG layers that correspond to a board's layers.
-    Includes the default style definition from the stylesheet.
+    Includes the default style definition from the stylesheet as a 'class'.
     Returns a dictionary of layer instantiations.
     """
-
-    ns_pcm = config.cfg["ns"]["pcbmode"]
-    ns_sp = config.cfg["ns"]["sodipodi"]
-
-    # Contains whether to show, place, or lock each layer
-    layer_control = config.cfg["layer-control"]
-
     layers = {}
 
     # Create layers for the PCB according to the stackup. We need to reverse the
     # order so that Inkscape shows them in the 'correct' order.
     for layer_dict in reversed(config.stk["layers-dict"]):
 
-        layer_name = layer_dict["name"]       
+        layer_name = layer_dict["name"]
         layers[layer_name] = {}
 
         # Create the primary layers ('top', 'bottom', etc.)
         # These have no 'style_class' or 'style'
-        el = layers[layer_name]["layer"] = create_layer(
+        layers[layer_name]["layer"] = create_layer(
             parent=parent_el,
-            name=layer_name,
+            name=layer_dict["name"],
             transform=transform,
             style_class=None,
             refdef=refdef,
+            pcbmode_prop="pcb-layer",
+            pcbmode_value=layer_dict["name"],
         )
-        el.set(f"{{{ns_pcm}}}pcb-layer", layer_name)
 
         # Each PCB layer is composed of 'sheets' (silkscreen, soldermask, etc.)
         # but also 'solderpaste' and 'assembly' and even 'placement' are part of
@@ -60,96 +54,93 @@ def create_layers(parent_el, transform=None, refdef=None):
         sheets = layer_dict["stack"]
 
         for sheet in reversed(sheets):
-
             sheet_type = sheet["type"]
- 
-            if layer_control[sheet_type]["hide"] == True:
-                style = "display:none;"
-            else:
-                style = None
 
-            print(f"{layer_name} {sheet_type}")
-
-            tmp = layers[layer_name]
-            tmp[sheet_type] = {}
- 
-            el = tmp[sheet_type]["layer"] = create_layer(
-                parent=tmp["layer"],
+            layers[layer_name][sheet_type] = {}
+            layers[layer_name][sheet_type]["layer"] = create_layer(
+                parent=layers[layer_name]["layer"],
                 name=sheet["name"],
                 transform=None,
                 style_class=f"{layer_name}-{sheet_type}",
                 refdef=refdef,
+                pcbmode_prop="sheet",
+                pcbmode_value=sheet["type"],
+                lock=config.cfg["layer-control"][sheet["type"]].get("lock", False),
+                hide=config.cfg["layer-control"][sheet["type"]].get("hide", False),
             )
 
-            el.set(f"{{{ns_pcm}}}sheet", sheet_type)
- 
-            if layer_control[sheet_type]["lock"] == True:
-                el.set(f"{{{ns_sp}}}insensitive", "true")
-
-            # A PCB layer of type 'conductor' is best presented in
-            # seperate sub-layers of 'pours', 'pads', and
-            # 'routing'. The following generates those sub-layers
+            # Sheet 'conductor' also gets sub-layers 'pours', 'pads', and 'routing'.
             if sheet_type == "conductor":
-                tmp2 = layers[layer_name]["conductor"]
                 conductor_types = ["routing", "pads", "pours"]
-
                 for cond_type in conductor_types:
-                    if layer_control["conductor"][cond_type]["hide"] == True:
-                        style = "display:none;"
-                    else:
-                        style = None
-
-                    tmp2[cond_type] = {}
-                    el = tmp2[cond_type]["layer"] = create_layer(
-                        parent=tmp2["layer"],
+                    layers[layer_name]["conductor"][cond_type] = {}
+                    layers[layer_name]["conductor"][cond_type]["layer"] = create_layer(
+                        parent=layers[layer_name]["conductor"]["layer"],
                         name=cond_type,
                         transform=None,
                         style_class=layer_name,
                         refdef=refdef,
+                        pcbmode_prop="sheet",
+                        pcbmode_value=cond_type,
+                        lock=config.cfg["layer-control"]["conductor"][cond_type].get(
+                            "lock", False
+                        ),
+                        hide=config.cfg["layer-control"]["conductor"][cond_type].get(
+                            "hide", False
+                        ),
                     )
 
-                    el.set(f"{{{ns_pcm}}}sheet", cond_type)
-
-                    if layer_control["conductor"][cond_type]["lock"] == True:
-                        el.set(f"{{{ns_sp}}}insensitive", "true")
-
-    for info_layer in ["origin", "dimensions", "outline", "drills", "documentation"]:
-        # style = utils.dictToStyleText(config.stl["layout"][info_layer].get("default"))
-        if layer_control[info_layer]["hide"] == True:
-            style = "display:none;"
-        else:
-            style = None
-
+    info_layers = ["drills", "outline", "origin", "dimensions", "documentation"]
+    for info_layer in info_layers:
         layers[info_layer] = {}
-        el = layers[info_layer]["layer"] = create_layer(
+        layers[info_layer]["layer"] = create_layer(
             parent=parent_el,
             name=info_layer,
             transform=transform,
             style_class=info_layer,
             refdef=refdef,
+            pcbmode_prop="sheet",
+            pcbmode_value=info_layer,
+            lock=config.cfg["layer-control"][info_layer].get("lock", False),
+            hide=config.cfg["layer-control"][info_layer].get("hide", False),
         )
-        el.set(f"{{{ns_pcm}}}sheet", info_layer)
-        if layer_control[info_layer]["lock"] == True:
-            el.set(f"{{{ns_sp}}}insensitive", "true")
 
     return layers
 
 
-def create_layer(parent, name, transform=None, style_class=None, refdef=None):
+def create_layer(
+    parent,
+    name,
+    transform=None,
+    style_class=None,
+    refdef=None,
+    pcbmode_prop=None,
+    pcbmode_value="",
+    lock=False,
+    hide=False,
+):
     """
     Create and return an Inkscape SVG layer 
     """
 
     ns_ink = config.cfg["ns"]["inkscape"]
+    ns_pcm = config.cfg["ns"]["pcbmode"]
+    ns_sp = config.cfg["ns"]["sodipodi"]
 
     new_layer = et.SubElement(parent, "g")
     new_layer.set(f"{{{ns_ink}}}groupmode", "layer")
     new_layer.set(f"{{{ns_ink}}}label", name)
+    if pcbmode_prop is not None:
+        new_layer.set(f"{{{ns_pcm}}}{pcbmode_prop}", pcbmode_value)
     if transform is not None:
         new_layer.set("transform", transform)
     if style_class is not None:
         new_layer.set("class", style_class)
     if refdef is not None:
         new_layer.set("refdef", refdef)
+    if lock == True:
+        new_layer.set(f"{{{ns_sp}}}insensitive", "true")
+    if hide == True:
+        new_layer.set("style", "display:none;")
 
     return new_layer
