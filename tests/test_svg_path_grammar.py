f83ead69c26b8572d696eb39f9e2dddf5f2efcb9
Saar Drimer <saardrimer@gmail.com>
2020-01-17 14:50:56 +0000

    License header

--- tests/test_svg_path_grammar.py
+++ tests/test_svg_path_grammar.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python
 
+#!/usr/bin/python
+
+# PCBmodE, a printed circuit design software with a twist
+# Copyright (C) 2020 Saar Drimer
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import unittest
 
 from pcbmode.utils.svg_path_grammar import get_grammar
